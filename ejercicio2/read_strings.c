2872790f87973714672ad5e0bbc694419227d71e
bryanX02 <bryanxqu@ucm.es>
2025-01-28 22:22:14 +0100

    Ejercicio 1 completado

---
+++ ejercicio2/read_strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+
+/** Loads a string from a file.
+ *
+ * fd: integer file descriptor
+ *
+ * The loadstr() function must allocate memory from the heap to store
+ * the contents of the string read from the FILE.
+ * Once the string has been properly built in memory, the function returns
+ * the starting address of the string (pointer returned by malloc())
+ *
+ * Returns: !=NULL if success, NULL if error
+ */
+char *loadstr(int fd)
+{
+	/* To be completed */
+	return NULL;	
+}
+
+int main(int argc, char *argv[])
+{
+	/* To be completed */
+	return 0;
+}
