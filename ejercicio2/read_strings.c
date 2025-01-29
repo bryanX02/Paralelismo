8f56f645a393d558d945bf023a3796f08df7fbd6
DanielColeto <118129918+DanielColeto@users.noreply.github.com>
2025-01-29 08:11:00 +0000

    Terminado a falta de revision "write_strings" - "read_strings" a medias

--- ejercicio2/read_strings.c
+++ ejercicio2/read_strings.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <err.h>
+#include<fcntl.h>
 
 /** Loads a string from a file.
  *
@@ -16,12 +17,55 @@
  */
 char *loadstr(int fd)
 {
-	/* To be completed */
+	
+	int tam;
+	int i = 0;
+	char buffer;
+	tam = read(fd,&buffer,1);
+	if(tam==0){
+		return 0;
+	}
+
+	while(buffer!='\0'){
+		i++;
+		tam = read(fd,&buffer,1);
+		printf("%c\n", buffer);
+		printf("tam tam -- %d\n", tam);
+	}
+	
+	
+	printf("%d\n", i);
+	char *cadena = malloc(i + 1);
+
+	off_t of = lseek(fd,-i - 1,SEEK_CUR);
+
+	printf("of -- %ld\n", of);
+	
+	tam = read(fd, cadena, i + 1);
+
+	printf("tam -- %d\n", tam);
+
+	cadena[i] = '\0';
+	printf("cadena -- %s\n", cadena);
+
 	return NULL;	
 }
 
 int main(int argc, char *argv[])
 {
 	/* To be completed */
+	int fd;
+	fd = open(argv[1],O_RDONLY);
+
+	char *cadena = loadstr(fd);
+
+	/*
+	cadena = loadstr(fd);
+
+	cadena = loadstr(fd);
+	cadena = loadstr(fd);
+	cadena = loadstr(fd);
+	cadena = loadstr(fd);
+	*/
 	return 0;
 }
--- ejercicio2/write_strings.c
+++ ejercicio2/write_strings.c
@@ -2,9 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <err.h>
+#include<fcntl.h>
 
 int main(int argc, char* argv[])
 {
-	/* To be completed */
+	//printf("%s - %s - %s\n", argv[2], argv[3], argv[4]);
+	int fd_in;
+	//fd_in = creat(argv[1],0751);
+	fd_in = open(argv[1],O_WRONLY|O_CREAT|O_TRUNC,0751);
+	if (fd_in< 0){
+		perror("creat"); exit(1);
+ 	}
+	int i = 2;
+	while(argv[i]!=NULL){
+		//printf("%s\n", argv[i]);
+		int len_i = strlen(argv[i]);
+		char buffer[len_i];
+		strcpy(buffer, argv[i]);
+		//buffer = argv[i];
+		//printf("%s\n", buffer);
+
+		if(write(fd_in,buffer,len_i)!=len_i){
+			perror("write");
+ 			close(fd_in);
+ 			exit(1);
+ 			
+		}
+		char fin = '\0';
+		if(write(fd_in,&fin,1)!=1){
+			perror("write");
+ 			close(fd_in);
+ 			exit(1);
+ 			
+		}
+
+		i++;
+	}
+	
+
 	return 0;
 }
